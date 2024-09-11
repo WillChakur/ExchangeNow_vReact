0d09bac9e9900a007c28c506b25e5ca6b2183909
William de Morais Chakur <wmoraischakur71@gmail.com>
2024-09-11 11:56:14 -0300

    Add: implementing jwt decode to check if the token has expired

--- frontend/fetchData/fetchUser.js
+++ frontend/fetchData/fetchUser.js
@@ -1,6 +1,26 @@
+import jwt_decode from "jwt-decode";
+
 const fetchUser = () => {
   const token = localStorage.getItem("token");
-  return !!token;
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const { exp } = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+
+    if (exp < currentTime) {
+      localStorage.removeItem("token");
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Invalid Token: ", error);
+    return false;
+  }
 };
 
 export default fetchUser;
