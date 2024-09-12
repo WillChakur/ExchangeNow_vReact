327eba42b8cef33c1e59bfddfc089797ac72a8e8
William de Morais Chakur <wmoraischakur71@gmail.com>
2024-09-12 19:16:56 -0300

    Add: Procfile to deploy on heroku and heroku postgresql

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const routerRates = require("./routes/rates");
 const routerUser = require("./routes/user");
@@ -17,6 +18,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(express.static(path.join(__dirname, "../", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "dist", "index.html"));
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
@@ -24,7 +30,7 @@ app.use(cookieParser());
 app.use("/rates", routerRates);
 app.use("/user", routerUser);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
